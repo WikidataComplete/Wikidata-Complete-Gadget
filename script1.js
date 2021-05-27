1dc65024da7c627c80031ef813eca869919f0b14
Dhairya3124 <dk9582671597@gmail.com>
2021-05-28 05:22:27 +0530

    Completed Recoin Integration

--- script1.js
+++ script1.js
@@ -7,21 +7,40 @@
 // 5. Show it
 let userinput = document.getElementById("userinput").value
 let btn = document.querySelector("button")
-btn.onclick = datawiki
 
 console.log(userinput);
-
+var recoin_val
 let url = "https://www.wikidata.org/w/api.php?action=wbsearchentities&origin=*&format=json&search="
 let language = "&language=en"
 let val = userinput
 let result1 = url + val + language
-
-async function datawiki(){
-  //JSON.parse(theStringThatIsNotJson)
+//async function datawiki(){
+  const datawiki = async () => {
   console.log(result1)
   const response  = await fetch(result1)
   const data = await response.json()
-  console.log(data)
-  
+  //console.log(data)
+  //console.log(data)
+  const {searchinfo,search,search_continue} = data
+  console.log(search[0].id)
+  recoin_val = search[0].id
+    return recoin_val
 }
-//btn.onclick = datawiki
+const recoin = async url2 =>{
+  const request = await fetch(url2)
+  const data2 = await request.json()
+  // request.addHeader("Access-Control-Allow-Origin", "*")
+  // request.addHeader("Access-Control-Allow-Origin", url2)
+  
+  //console.log(data2)
+  return data2
+}
+
+const main = async() =>{
+  const ans = await datawiki()
+
+  let url2 = " https://tools.wmflabs.org/recoin/getmissingattributes.php?lang=en&format=json&origin=*&subject=" + ans +"&n=10"
+  const final = await recoin(url2)
+  console.log(final)
+}
+btn.onclick = main
